9aa6df6651cb14da5cd189cf050312dc23f029a7
MoazMansour <moaz.mansour@gmail.com>
2019-04-09 23:37:07 -0400

    Binary search with recursion

--- binary_search.py
+++ binary_search.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 
 def binarySearch(arr, value):
+    if not arr:
+        return -1
     start = 0
     end = len(arr)-1
-    while start != end:
-        mid = int((start+end)/2)
-        if arr[mid] == value:
-            return mid
-        if arr[mid] > value:
-            start = start
-            end = mid - 1
-            continue
-        else:
-            start = mid + 1
-            end = end
-    if arr[start] == value:
-        return start
-    return -1
+    mid = int((start+end)/2)
+    if arr[mid] == value:
+        return mid
+    if arr[mid] > value:
+        start = start
+        end = mid
+    else:
+        start = mid + 1
+        end = end + 1
+    index = binarySearch(arr[start:end], value)
+    if index < 0:
+        return -1
+    return start + index
+
 
 
 def main():
     arr = range(12)
-    print(binarySearch(arr, 11))
+    print(binarySearch(arr, 3))
 
 
 if __name__ == '__main__':
